9c27ad705c8ff77a21b797e5bc8b65f0b9689bd9
edh <edector@nuvek.com>
2018-10-08 18:07:07 -0500

    Numeros en ruby

---
+++ numbers.rb
@@ -0,0 +1,16 @@
+#test programming edh:
+
+#how many hours are in a year?
+# se multiplica 365 dias por 24 horas
+puts 365 * 24
+#how many minutes are in a decade?
+# 1 decada tiene 10 años, se multiplica por 365 dias por 24 horas por 60 minutos
+puts 10 * 365 * 24 * 60
+#how many seconds old are you?
+# solo agrego una multiplicacion por 60 para obtener los segundos
+puts 30 * 365 * 24 * 60 * 60
+#how many chocolates do you hope to eat in your life? 
+puts 10
+#If I am 1298 million seconds old, how old am I? 
+#paso los segundos a horas luego a dias y luego a años
+puts 1298000000/3600/24/365
