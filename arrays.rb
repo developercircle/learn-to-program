8db238831e07d37d3d8ba41e9291c786d2856d25
edh <edector@nuvek.com>
2018-10-09 17:04:47 -0500

    Capitulo 7, arrays

---
+++ arrays.rb
@@ -0,0 +1,18 @@
+#edector
+
+palabras = []
+oracion = ''
+
+puts 'Escribe una lista de palabras, separadas por enter, termina con un espacio en blanco y presiona enter'
+
+
+while (oracion = gets.chomp)!= ''
+
+
+palabras.push oracion
+
+
+end
+
+puts 'array original: ' + palabras.join(', ')
+puts 'array ordenado: ' + palabras.sort.join(', ')
